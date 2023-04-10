8f17185144f7980f00a400eef0455a2308683a46
DavieK2 <daviekingz.dk@gmail.com>
2023-04-10 16:19:58 +0100

    Build Scraper

--- index.php
+++ index.php
@@ -4,52 +4,122 @@
 
     require __DIR__ .'/vendor/autoload.php';
 
-    $browser = new \Symfony\Component\BrowserKit\HttpBrowser(HttpClient::create());
-
-
     $link = $_POST['url'] ?? NULL;
+
+    function checkIfPageIsProperlyTranslate($browser, $link){
+
+        //Get links to other pages
+        try {
+
+
+            $b = $browser->request('GET', $link);
+
+            $image = $b->filter('#learning-illus > img')->attr('src');
+
+            if(filter_var($image, FILTER_VALIDATE_URL)){
+                
+                $img = get_headers($image, 1);
+
+               
+                if($img["Content-Length"]/1024 < 20){
+                    return [$link => [
+                        'errors' => ['Image resolution is not good']
+                    ]];
+                }
+            }
+
+            $title_text = $b->filter('title')->text();
+
+            $browser->request("GET", 'https://api.dandelion.eu/datatxt/li/v1?token=0ba45bb4edfe4a51951288128b98f429&text='.$title_text);
+
+            if($browser->getResponse()->toArray()['detectedLangs'][0]['lang'] != 'hi'){
+                return [$link => [
+                    'errors' => ['One or more pages are not translated properly']
+                ]];
+            }
+
+            $links = $b->filter('a')->each(function($node, $i) {
+                return $node->link()->getUri(); 
+              });
+  
+              $totalLinks = (count($links));
+  
+              $randomLinks = [];
+  
+              for ($i=0; $i < 5; $i++) { 
+                  $randomLinks[] = $links[rand(0, $totalLinks)];
+              }
+
+              foreach($randomLinks as $randomLink){
+                    $b = $browser->request('GET', $randomLink);
+                    $title_text = $b->filter('title')->text();
+                    $browser->request("GET", 'https://api.dandelion.eu/datatxt/li/v1?token=0ba45bb4edfe4a51951288128b98f429&text='.$title_text);
+
+                    if($browser->getResponse()->toArray()['detectedLangs'][0]['lang'] != 'hi'){
+                        return [$link => [
+                            'errors' => ['One or more pages are not translated properly']
+                        ]];
+                    }
+
+                    break;
+              }
+
+              return [$link => [
+                'status' => 'Passed'
+            ]];
+
+
+        } catch (\Throwable $th) {
+                return [
+                    $link => [
+                        'errors' => ['Website is not working properly']
+                    ]
+                ];
+        }
+    }
 
     if($link){
 
-        $file_name = 'books.csv';
-        // chmod($file_name, 0644);
-        // $book = fopen($file_name, 'a');
 
-        while($link){
+        $browser = new \Symfony\Component\BrowserKit\HttpBrowser(HttpClient::create());
 
-            $b = $browser->request('GET', $link);
+        $form_links = preg_split('/\s{1,}/',$link);
+
+        $form_links = array_filter($form_links, fn($item) => $item != '');
+
+        if(count($form_links) < 1) {
+
+            echo 'Invalid link';
+
+            die();
+        }
     
-            $b->filter('h3')->each(function($node) {
-                $title = $node->filter('a')->attr('title');
+        $translation_status = [];
 
-                echo '<pre>';
-                echo $title;
-                echo '</pre>';
-                // fputcsv($book, [$title]);
-            } );
-    
-            try {
-                $link = $b->filter('.next > a')->link()->getUri() ?? null;
-            } catch (\Throwable $th) {
-                $link = null;
-            }
+        foreach ($form_links as $key => $link) {
+           
+            $translation_status[] = checkIfPageIsProperlyTranslate($browser, $link);
+            
         }
 
-        // $mime_type = mime_content_type($file_name);
-        // $file_size = filesize($file_name);
+       foreach($translation_status as $key =>  $statuses){
 
-        // header("Content-Type: $mime_type");
-        // header("Content-Length: $file_size");
-        // header("Content-Disposition: attachment; filename=$file_name");
-        // header("Expires: 0");
-        // header('Cache-Control: must-revalidate');
-        // header('Pragma: public');
-        
-        // readfile($file_name);
-        
-        // fclose($book);
+            echo '<pre>';
+            
+            foreach($statuses as $key => $status){
+                if(isset($status['errors'])){
 
-        exit();
+                    echo '<h3>'.$key. ': Fail: ('.implode(', ', $status['errors']).')</h3>';
+                }
+                else{
+                    echo '<h3>'.$key. ': ('.implode(', ', $status).')</h3>';
+                }
+            }
+
+            echo '</pre>';
+        }
+
+        die();
     }
     
 ?>
@@ -67,7 +137,7 @@
     <h1>Enter Website Name</h1>
     <br>
     <form method="post">
-        <input name="url" type="text">
+        <textarea name="url" id="" cols="30" rows="10"></textarea>
         <button>Scrape</button>
     </form>
 </body>
